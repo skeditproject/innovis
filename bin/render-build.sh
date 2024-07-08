4f13e9a22c005dfcb5af54b266a88dd7d83f2f78
skeditproject <skeditproject@gmail.com>
2024-07-08 17:30:23 +0530

    Updated build file for render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,8 +3,6 @@
 set -o errexit
 
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
 bundle exec rake generate camaleon_cms:install
 bundle exec rake camaleon_cms:generate_migrations
 bundle exec rake db:migrate
