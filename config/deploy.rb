e1de86e9ce9cddc57b193c2e4b14729859493403
skeditproject <skeditproject@gmail.com>
2024-07-17 18:29:27 +0530

    Added command for ruby

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,6 +15,7 @@ set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :rvm_ruby_version, '3.0.0'
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_rackup,     "#{release_path}/config.ru"
 set :puma_access_log, "#{release_path}/log/puma.access.log"
