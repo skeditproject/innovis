2f8f27e5a7e453cce176426165214a5b5abf344a
skeditproject <skeditproject@gmail.com>
2024-09-11 15:29:53 +0530

    Updated form for blog posts

--- app/models/count_section.rb
+++ app/models/count_section.rb
@@ -4,10 +4,6 @@ class CountSection < ApplicationRecord
   has_one_attached :image
 
   def self.ransackable_attributes(auth_object = nil)
-    super + %w[created_at updated_at]
-  end
-
-  def self.ransackable_associations(auth_object = nil)
-    super + %w[image_attachment image_blob]
+    ["created_at", "id", "id_value", "number", "text", "updated_at"]
   end
 end
